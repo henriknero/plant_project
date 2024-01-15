098c56e4012b9c7fb359c6b6d8f8891b01aa066a
Henrik Nero <henrik.nero@knowit.se>
2024-01-15 21:43:00 +0100

    Putting it all together and add snore lib for deep sleep

--- ATTiny85_moisture_sensor/ATTiny85_moisture_sensor.ino
+++ ATTiny85_moisture_sensor/ATTiny85_moisture_sensor.ino
@@ -1,4 +1,5 @@
 #include "SoftwareSerial.h"
+#include "tinysnore.h"
 SoftwareSerial SoftSerial(3,4);
 void setup() {
   SoftSerial.begin(9600);
@@ -8,30 +9,29 @@ void setup() {
   pinMode(2, INPUT);
   pinMode(1, OUTPUT);
   pinMode(0, OUTPUT);
-  digitalWrite(1, HIGH);
-
   // prints title with ending line break
   SoftSerial.println("Setup done");
 }
 
 void loop() {
-    digitalWrite(0,HIGH);
-    delay(5000);
-    digitalWrite(0,LOW);
-    delay(5000);
-  /*
-  delay(1000);
-  int sensorValue = analogRead(1);
-  SoftSerial.println(sensorValue);
+  //int sensorValue = readMoisture();
+  snore(5000); //7microAmpere
+  delay(5000); //7milliAmpere
+  /*SoftSerial.println(sensorValue);
   if(sensorValue > 500){
     digitalWrite(0,HIGH);
     delay(5000);
     digitalWrite(0,LOW);
   }
   */
-
 }
 
+int readMoisture(){
+  digitalWrite(1, HIGH);
+  delay(50);
+  return analogRead(1);
+  digitalWrite(1,LOW);
+}
 
 //The analog and digital pin numbers are not the same.
 //PB3 is analog pin 3, that's why it works.
