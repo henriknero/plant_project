a1e122bc8decfb78e6fea61dfb24d48d558c4d53
Henrik Nero <henriknero@gmail.com>
2024-01-29 21:07:13 +0100

    Added Moisture Sensitivity to EEPROM

--- ATTiny85_moisture_sensor/ATTiny85_moisture_sensor.ino
+++ ATTiny85_moisture_sensor/ATTiny85_moisture_sensor.ino
@@ -5,38 +5,29 @@ SoftwareSerial SoftSerial(3,4);
 unsigned long SECONDS = 1000;
 unsigned long MINUTES = SECONDS*60;
 unsigned long PUMP_ACTIVE_TIME = 5000;
-byte value = 0;
+byte MOISTURE_SENSITIVITY = EEPROM.read(511);
 
 void setup() {
+  pinMode(2, INPUT); pinMode(1, OUTPUT); pinMode(0, OUTPUT);
+  
   SoftSerial.begin(9600);
-  while (!SoftSerial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
-  pinMode(2, INPUT);
-  pinMode(1, OUTPUT);
-  pinMode(0, OUTPUT);
-  SoftSerial.println("Setup done");
-  for (int address=0; address < EEPROM.length(); address++){
-    value = EEPROM.read(address);
-    SoftSerial.print(address);
-    SoftSerial.print("\t");
-    SoftSerial.print(value, DEC);
-    SoftSerial.println();
-    }
+  while (!SoftSerial) {;} // wait for serial port to connect. Needed for native USB port only
+  SoftSerial.print("Moisture Sensitivity: "); SoftSerial.println(MOISTURE_SENSITIVITY);
 }
 
 void loop() {
   byte sensorValue = readMoisture();
   logSensorValue(sensorValue);
-  //if(sensorValue > 167)
-  //  runPump();
-  sleepForAWhile();
+  if(sensorValue > MOISTURE_SENSITIVITY)
+    runPump();
+  delay(60*MINUTES);
+  //snore(time); // Use when on battery
 }
 
 byte readMoisture(){
   digitalWrite(1, HIGH);
   delay(2500);
-  byte sensorValue = analogRead(1) / 3; // Value of Sensor never exceeds 750. 750/3=250 which is lower than max value of a byte so I can store many days at a time.
+  byte sensorValue = analogRead(1) / 3; // Value of Sensor never exceeds 750 (when on 5v). 750/3=250 which is lower than max value of a byte so I can store many days at a time.
   digitalWrite(1, LOW);
   return sensorValue;
   //The analog and digital pin numbers are not the same.
@@ -51,23 +42,14 @@ void runPump(){
     digitalWrite(0,LOW);
 }
 
-void sleepForAWhile(){
-  SoftSerial.println("Starting sleep");
-  delay(60*MINUTES);
-  //snore(60*MINUTES); // Use when on battery
-  SoftSerial.println("Sleep done");
-}
-
 void logSensorValue(byte sensorValue){
-  int currentLogIndex = readIntFromEEPROM(0);
-  SoftSerial.print(currentLogIndex);
-  SoftSerial.print(": ");
-  SoftSerial.println(sensorValue);
-  if (currentLogIndex == 512) //Rotate back to First index (2 since we store current index at 0 and 1)
-    currentLogIndex = 2;
+  unsigned int currentLogIndex = readIntFromEEPROM(509);
+  if (currentLogIndex >= 509) //Rotate back to index 0 to now overwrite current index and moisture sensitivity value
+    currentLogIndex = 0;
+  SoftSerial.print(currentLogIndex); SoftSerial.print(": "); SoftSerial.println(sensorValue);
   EEPROM.write(currentLogIndex, sensorValue); //Don't overwrite currentLogIndex in EEPROM
   currentLogIndex++;
-  writeIntIntoEEPROM(0, currentLogIndex);
+  writeIntIntoEEPROM(509, currentLogIndex);
 }
 
 void writeIntIntoEEPROM(int address, int number)
@@ -76,7 +58,7 @@ void writeIntIntoEEPROM(int address, int number)
   EEPROM.write(address + 1, number & 0xFF);
 }
 
-int readIntFromEEPROM(int address)
+unsigned int readIntFromEEPROM(int address)
 {
   byte byte1 = EEPROM.read(address);
   byte byte2 = EEPROM.read(address + 1);
